eed2d996bf90478bdc96b0aaaa111759db191b28
EGA-SUPREMO <cxiesestro@gmail.com>
2024-03-26 09:36:51 -0400

    add consultas en comentarios en basedatos

--- video_club/src/basedatos/Consulta.java
+++ video_club/src/basedatos/Consulta.java
@@ -4,6 +4,47 @@ import java.sql.*;
 
 /**
  *
+ * -- Inicio de sesion
+SELECT *
+FROM socio
+WHERE id = 'cedula_del_socio';
+
+
+-- Para consultas del buscador
+SELECT *
+FROM pelicula
+WHERE titulo = 'título_de_la_pelicula';
+
+SELECT p.*
+FROM pelicula p
+JOIN genero g ON p.id_genero = g.id
+WHERE g.nombre = 'terror';
+
+SELECT p.*
+FROM pelicula p
+JOIN actores_en_peliculas ap ON p.id = ap.id_pelicula
+JOIN actor a ON ap.id_actor = a.id
+WHERE a.nombre = 'nombre_del_actor';
+
+SELECT p.*
+FROM pelicula p
+JOIN director d ON p.id_director = d.id
+WHERE d.nombre = 'nombre_del_director';
+
+-- para cuando se muestra en las busquedas, tiene dos vars de input(el id de la pelicula), en la columna de cintas disponibles
+SELECT COUNT(*) AS num_cintas_disponibles
+FROM cinta
+WHERE id_pelicula = 4 AND id NOT IN 
+(
+    SELECT cinta.id
+    FROM cinta
+    JOIN prestamo_cinta ON cinta.id = prestamo_cinta.id_cinta
+    JOIN prestamo ON prestamo_cinta.id_prestamo = prestamo.id
+    JOIN pelicula ON cinta.id_pelicula = pelicula.id
+    WHERE pelicula.id = 4 AND prestamo.fecha_devuelta IS NULL
+);
+
+
  * @author alejandro
  */
 public class Consulta {
