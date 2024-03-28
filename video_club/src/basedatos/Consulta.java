02341305d33305436bdc6e806238ea950bd8f849
EGA-SUPREMO <cxiesestro@gmail.com>
2024-03-28 16:31:44 -0400

    add metodos de para consultas relacionados al socio

--- video_club/src/basedatos/Consulta.java
+++ video_club/src/basedatos/Consulta.java
@@ -5,25 +5,20 @@ import video_club.Pelicula;
 import java.sql.*;
 
 /**
- *
  * -- Inicio de sesion
-SELECT *
-FROM socio
-WHERE id = 'cedula_del_socio';
-
-borrar cuenta
-
-quitar 90 al socio actual
-
--- Para consultas del buscador
-*hecho
-
--- ver lista de espera
-
+ * usa obtenerSocio()
+ * quitar 90 al socio por id se usa el metodo restarSaldo()
+ * 
+ * -- ver lista de espera, se pide id de socio y se muestra, titulo y estado
+ * 
+ * 
+ * -- Para consultas del buscador
+ * usa buscarPor + lo que haya selecionado el usuario, ej buscarPorGenero(String) para obtener todas las peliculas que tengan ese genero
+ * 
  * @author alejandro
  */
 public class Consulta {
-    Connection connection;
+    private Connection connection;
     
     public Consulta(String url, String username, String password) throws SQLException {
         connection = DriverManager.getConnection(url, username, password);
@@ -42,7 +37,11 @@ public class Consulta {
             Pelicula peli = consulta.buscarPorTitulo("The Godfather")[0];
             //Pelicula peli = consulta.buscarPorGenero("Drama")[0];
             //System.out.println(consulta.buscarPorGenero("Drama").length);
-
+            System.out.println(consulta.obtenerSocio(2)[0]);
+            System.out.println(consulta.obtenerSocio(2)[1]);
+            System.out.println(consulta.restarSaldo(2));
+            consulta.borrarCuenta(2);
+            System.out.println(consulta.obtenerSocio(2));
             System.out.println(peli.genero);
             System.out.println(peli.director);
             System.out.println(peli.actores[0]);
@@ -57,7 +56,67 @@ public class Consulta {
         }
     }
 
-    private Pelicula[] buscarPorTitulo(String titulo) throws SQLException {
+    public boolean restarSaldo(int id) {
+        String sql = "SELECT saldo FROM socio WHERE id = ?";
+        
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    int saldoActual = resultSet.getInt("saldo");
+                    if (saldoActual - 90 < 0) {
+                        return false;
+                    } else {
+                        String updateSql = "UPDATE socio SET saldo = saldo - 90 WHERE id = ?";
+                        try (PreparedStatement updateStatement = connection.prepareStatement(updateSql)) {
+                            updateStatement.setInt(1, id);
+                            int rowsAffected = updateStatement.executeUpdate();
+                            
+                            return rowsAffected > 0;
+                        }
+                    }
+                } else {
+                    return false;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public String[] obtenerSocio(int id) {
+        String[] socio = new String[2];
+        String sql = "SELECT nombre, saldo FROM socio WHERE id = ?";
+        
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            
+            if (resultSet.next()) {
+                String nombre = resultSet.getString("nombre");
+                String saldo = resultSet.getInt("saldo") + "";
+                socio[0] = saldo;
+                socio[1] = nombre;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return socio;
+    }
+
+
+    public void borrarCuenta(int id) throws SQLException{
+        String sql = "DELETE FROM socio WHERE id = ?";
+        
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, id+"");
+            ResultSet resultSet = statement.executeQuery();
+        }
+    }
+
+    public Pelicula[] buscarPorTitulo(String titulo) throws SQLException {
         String sql = "SELECT * FROM pelicula WHERE titulo = ?";
         
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -68,7 +127,7 @@ public class Consulta {
         }
     }
     
-    private Pelicula[] buscarPorGenero(String genero) throws SQLException {
+    public Pelicula[] buscarPorGenero(String genero) throws SQLException {
         String sql = "SELECT p.* FROM pelicula p JOIN genero g ON p.id_genero = g.id WHERE g.nombre = ?";
         
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -79,7 +138,7 @@ public class Consulta {
         }
     }
     
-    private Pelicula[] buscarPorActor(String nombreActor) throws SQLException {
+    public Pelicula[] buscarPorActor(String nombreActor) throws SQLException {
         String sql = "SELECT p.* FROM pelicula p JOIN actores_en_peliculas ap ON p.id = ap.id_pelicula " +
                      "JOIN actor a ON ap.id_actor = a.id WHERE a.nombre = ?";
         
@@ -91,7 +150,7 @@ public class Consulta {
         }
     }
     
-    private Pelicula[] buscarPorDirector(String nombreDirector) throws SQLException {
+    public Pelicula[] buscarPorDirector(String nombreDirector) throws SQLException {
         String sql = "SELECT p.* FROM pelicula p JOIN director d ON p.id_director = d.id WHERE d.nombre = ?";
         
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
@@ -129,10 +188,11 @@ public class Consulta {
     }
 
     private String cintas_disponiblesPorPeliculaId(int id) throws SQLException {
-        String sql = "SELECT COUNT(*) AS num_cintas_disponibles FROM cinta WHERE id_pelicula = ? AND id NOT IN (SELECT cinta.id FROM cinta JOIN prestamo_cinta ON cinta.id = prestamo_cinta.id_cinta JOIN prestamo ON prestamo_cinta.id_prestamo = prestamo.id JOIN pelicula ON cinta.id_pelicula = pelicula.id WHERE pelicula.id = 4 AND prestamo.fecha_devuelta IS NULL );";
+        String sql = "SELECT COUNT(*) AS num_cintas_disponibles FROM cinta WHERE id_pelicula = ? AND id NOT IN (SELECT cinta.id FROM cinta JOIN prestamo_cinta ON cinta.id = prestamo_cinta.id_cinta JOIN prestamo ON prestamo_cinta.id_prestamo = prestamo.id JOIN pelicula ON cinta.id_pelicula = pelicula.id WHERE pelicula.id = ? AND prestamo.fecha_devuelta IS NULL );";
         
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, id+"");
+            statement.setString(2, id+"");
             ResultSet rs = statement.executeQuery();
             if (rs.next()) {
                 return rs.getString("num_cintas_disponibles");
